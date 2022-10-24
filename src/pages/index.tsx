81b706cbac2f3ebd71dd87269cce7d38e7a41fcf
Enes Ünlüer <enes@kleuize.com>
2022-10-24 16:09:37 +0300

    feat: home page header completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import axios from "axios";
 import type { NextPage } from "next";
+//3rd party
+import axios from "axios";
 //UI
 import Head from "next/head";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
+import Image from "next/image";
+import {
+  Grid,
+  Box,
+  Container,
+  Stack,
+  Typography,
+  Button,
+  Divider,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
 //Component
 import { CourseCards } from "../components/cards/CourseCards";
-import GlobalStyles from "@mui/material/GlobalStyles";
-import CssBaseline from "@mui/material/CssBaseline";
 
 export async function getServerSideProps() {
   const { data } = await axios.get(`${process.env.API}/courses`);
@@ -25,40 +31,109 @@ export async function getServerSideProps() {
 
 export const Home: NextPage = ({ courses }: any) => {
   return (
-    <Container sx={{ mt: 10 }}>
+    <Container sx={{ mt: 15 }}>
       <Head>
         <title>Kleuize</title>
         <meta name="description" content="Learned English by Kleuize" />
       </Head>
-      <Container component="main">
-        <h1 className={styles.title}>
-          Welcome to{" "}
-          <a className={styles.title} href="/">
-            Kleuize
-          </a>
-        </h1>
-        <p className={styles.description}>
-          Öğrenmeye Başlayın <code className={styles.code}>Tüm Eğitimler</code>
-        </p>
-        <Container>
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="space-between"
-            mb={5}
+      <Grid container xs={12} spacing={1} mb={10}>
+        <Grid
+          container
+          xs={12}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={6}>
+            <Stack justifyContent="center" alignItems="center" ml={4}>
+              <Typography
+                variant="h1"
+                fontSize={38}
+                fontWeight="bold"
+                color="#08104D"
+              >
+                {/* <a href="/">Kleuize</a>  */}
+                Öğrenmenin daha fazlasına göz atın.
+              </Typography>
+              <br />
+              <Typography
+                variant="h5"
+                color="#6C708E"
+                fontWeight="bold"
+                component="p"
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard.
+              </Typography>
+            </Stack>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Stack justifyContent="center" alignItems="center">
+              <Image
+                alt="header-image"
+                src="/slider.svg"
+                width={600}
+                height={400}
+              />
+            </Stack>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography
+            variant="h2"
+            fontSize={20}
+            fontWeight="600"
+            marginY={5}
+            color="#6C708E"
           >
-            <Grid container spacing={3}>
-              {courses &&
-                courses.map((course: any) => (
-                  <div key={course._id} className={styles.grid}>
-                    <CourseCards course={course} />
-                  </div>
-                ))}
-            </Grid>
-          </Stack>
-        </Container>
-      </Container>
-      <footer></footer>
+            Haftanın öne çıkanları
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          sx={{
+            display: "flex",
+            flexWrap: "nowrap",
+            overflowX: "scroll",
+          }}
+        >
+          {courses &&
+            courses.map((course: any) => (
+              <div key={course._id}>
+                <CourseCards course={course} />
+              </div>
+            ))}
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            variant="h2"
+            fontSize={20}
+            fontWeight="600"
+            marginY={5}
+            color="#6C708E"
+          >
+            Tüm kurslar
+          </Typography>
+          <Grid
+            item
+            sx={{
+              display: "flex",
+              flexWrap: "nowrap",
+              overflowX: "scroll",
+            }}
+          >
+            {courses &&
+              courses.map((course: any) => (
+                <div key={course._id}>
+                  <CourseCards course={course} />
+                </div>
+              ))}
+          </Grid>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
